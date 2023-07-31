9b4c97466fd84816c8a086c78fabaed05ad65f18
Henrique Bonatto <nattohen@gmail.com>
2023-07-31 17:43:24 -0300

    commit 1

---
+++ app/src/main/java/com/nattodev/calculagasto/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.nattodev.calculagasto
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#44FF4B")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
---
+++ app/src/main/java/com/nattodev/calculagasto/ui/home/HomeFragment.kt
@@ -0,0 +1,56 @@
+package com.nattodev.calculagasto.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.nattodev.calculagasto.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View {
+            val homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+            _binding = FragmentHomeBinding.inflate(inflater, container, false)
+            val root: View = binding.root
+            binding.apply {
+                barChartMeses.animation.duration = animationDuration
+                barChartMeses.animate(barSet)
+            }
+            return root
+    }
+    companion object {
+        private val barSet = listOf(
+            "JAN" to 4F,
+            "FEV" to 8F,
+            "MAR" to 2F,
+            "MAI" to 2.3F,
+            "ABR" to 5F,
+            "JUN" to 4F,
+            "JUL" to 4F,
+            "AGO" to 7F,
+            "SET" to 2F,
+            "OUT" to 2.3F,
+            "NOV" to 5F,
+            "DEZ" to 4F
+        )
+        private const val animationDuration = 1000L
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
